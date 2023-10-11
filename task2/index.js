ead834e1fc0c9a2cc52beecc1443a258e10d8dce
Imdadul Ahmed <ahmedimdad1593@gmail.com>
2023-10-11 08:50:22 +0600

    finished task 1 & 2

--- task2/index.js
+++ task2/index.js
@@ -7,6 +7,20 @@
 // each_cons([1,2,3,4], 3)
 //   #=> [[1,2,3],[2,3,4]]
 
-function each_cons(array, n) {}
+function each_cons(array, n) {
+  let res = [];
+  for (let i = 0; i < array.length - n + 1; i++) {
+    let subRes = [];
+    for (let j = i; j < i + n; j++) {
+      const element = array[j];
+      subRes.push(element);
+    }
+    res.push(subRes);
+  }
+  return res;
+}
+
+// console.log(each_cons([1, 2, 3, 4], 2));
+// console.log(each_cons([1, 2, 3, 4], 3));
 
 module.exports = { each_cons };
